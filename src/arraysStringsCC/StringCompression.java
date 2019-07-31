08f7bffc58c6a4a16742d47ee54b1849a37f36ed
lesha K <alexei.jobs@gmail.com>
2019-07-31 08:39:21 +0300

    String compress 1.7?

--- src/arraysStringsCC/StringCompression.java
+++ src/arraysStringsCC/StringCompression.java
@@ -14,7 +14,7 @@ public class StringCompression {
         String compressedString = "";
         int countConsecutive = 0;
         for (int i= 0; i < str.length(); i++) {
-            countConsecutive++;
+            countConsecutive++;  // At start became 1 !!!
 /* If next character is different than current, append this char to result.*/
             if (i + 1 >= str.length() || str.charAt(i) != str.charAt(i + 1)) {
                 compressedString += "" + str.charAt(i) + countConsecutive;
@@ -26,9 +26,69 @@ public class StringCompression {
 /*    This works. ls it efficient, though?Take a look at the runtime of this code.
     The runtime is O(p + k2), where p is the size of the original string and k is thelnumber of character
     sequences. For example, if the string is aabccdeeaa, then there are six characte sequences. It's slow
-    because string concatenation operates in O(n2) time  */
+    because string concatenation operates in O(n2) time    */
 
-     public static void main(String[] args) {
+    public static void main(String[] args) {
+//         string aabcccccaaa would become a2blc5a3
+        String str = "aabcccccaaa";
+//        System.out.println("out: " + compressBad(str));
+//        System.out.println("out: " + compress(str));
+        System.out.println("out: " + compress2(str));
 
     }
+
+    //    ----------------------- Good one ----------------------------------------
+    public static String compress(String str) {
+        StringBuilder compressed= new StringBuilder();
+        int countConsecutive= 0;
+        for (int i= 0; i < str.length(); i++) {
+            countConsecutive++;
+
+            /* If next character is different than current, append this char to result.*/
+            if (i + 1 >= str.length() || str.charAt(i) != str.charAt(i + 1)) {
+                compressed.append(str.charAt(i));
+                compressed.append(countConsecutive);
+                countConsecutive= 0;
+            }
+        }
+        return compressed.length() < str.length() ? compressed.toString() : str;
+    }
+
+    //    ===============  Best One  =======================
+    public static String compress2(String str) {
+     /* Check final length and return input string if it would be longer. */
+        int finallength= countCompression(str);
+        if (finallength >= str.length()) return str;
+
+        StringBuilder compressed= new StringBuilder(finallength); // initial capacity
+        int countConsecutive = 0;
+        for (int i= 0; i < str.length(); i++) {
+            countConsecutive++;
+
+         /* If next character is different than current, append this char to result.*/
+            if (i + 1 >= str.length() || str.charAt(i) != str.charAt(i + 1)) {
+                compressed.append(str.charAt(i));
+                compressed.append(countConsecutive);
+                countConsecutive= 0;
+            }
+        }
+        return compressed.toString();
+    }
+
+    public static int countCompression(String str) {
+        int compressedLength= 0;
+        int countConsecutive= 0;
+        for (int i= 0; i < str.length(); i++) {
+            countConsecutive++;
+
+             /* If next character is different than current, increase the length.*/
+            if (i + 1 >= str.length() || str.charAt(i) != str.charAt(i + 1)) {
+                compressedLength  += 1 + String.valueOf(countConsecutive).length();
+                countConsecutive = 0;
+            }
+        }
+        return compressedLength;
+    }
+
+
 }
