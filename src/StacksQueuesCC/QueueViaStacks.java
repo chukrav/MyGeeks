26743c439b294305bb68453e69612f505d368efc
alexei <alexei.jobs@gmail.com>
2019-08-06 13:51:47 +0300

    Stacks ...
    Stacks queues in a middle
    
    SortStack start

---
+++ src/StacksQueuesCC/QueueViaStacks.java
@@ -0,0 +1,12 @@
+package StacksQueuesCC;
+
+/*3.4 Queue via Stacks: Implement a MyQueue class which implements a queue using two stacks.
+Since the major difference between a queue and a stack is the order (first-in first-out vs. last-in first-out), we
+know that we need to modify peek() and pop() to go in reverse order. We can use our second stack to
+reverse the order of the elements (by popping sl and pushing the elements on to s2). In such an implementation, on each peek() and pop() operation, we would pop everything from sl onto s2, perform
+the peek/pop operation, and then push everything back.
+pp 247
+ */
+
+public class QueueViaStacks {
+}
