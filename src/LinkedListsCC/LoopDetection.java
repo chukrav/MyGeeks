41e10230577a3ad108311881a5b7601a499bfe1e
alexei <alexei.jobs@gmail.com>
2019-08-06 13:51:47 +0300

    Stacks ...

--- src/LinkedListsCC/LoopDetection.java
+++ src/LinkedListsCC/LoopDetection.java
@@ -14,6 +14,22 @@ public class LoopDetection {
     This is a modification of a classic interview problem: detect if a linked list has a loop. Let's apply the Pattern
     Matching approach.
     An easy way to detect if a linked list has a loop is through the FastRunner / SlowRunner approach.
+    Floyd’s Cycle finding algorithm:
+    (m + n*x + k) = 2*(m + n*y + k)
+    Note that before meeting the point shown above, fast
+    was moving at twice speed.
+
+    x -->  Number of complete cyclic rounds made by
+           fast pointer before they meet first time
+    y -->  Number of complete cyclic rounds made by
+           slow pointer before they meet first time
+    m + k = (x-2y)*n
+    Which means m+k is a multiple of n.
+    So if we start moving both pointers again at same speed such that one pointer (say slow) begins
+     from head node of linked list and other pointer (say fast) begins from meeting point. When slow
+     pointer reaches beginning of loop (has made m steps), fast pointer would have made also moved m steps
+     as they are now moving same pace. Since m+k is a multiple of n and fast starts from k, they would meet
+     at the beginning. Can they meet before also? No because slow pointer enters the cycle first time after m steps.
 
      pp 234 */
 
--- src/StacksQueuesCC/StackMin.java
+++ src/StacksQueuesCC/StackMin.java
@@ -1,12 +1,90 @@
 package StacksQueuesCC;
 
+import java.util.Stack;
+
 /*3.2 Stack Min: How would you design a stack which, in addition to push and pop, has a function min
         which returns the minimum element? Push, pop and min should all operate in 0(1) time
+        To further understand this question, let's walk through it with a short example:
+        push(5); // stack is {5}, min is 5
+        push(6); // stack is {6, 5}, min is 5
+        push(3); // stack is {3, 6, 5}, min is 3
+        push(7); // stack is {7, 3, 6, 5}, min is 3
+        pop(); // pops 7. stack is {3, 6, 5}, min is 3
+        pop(); // pops 3. stack is {6, 5}. min is 5.
 
     pp 243    */
 public class StackMin {
 
+    public static class StackWithMin extends Stack<NodeWithMin> {
+        public void push(int value) {
+            int newMin = Math.min(value, min());
+            super.push(new NodeWithMin(value, newMin));
+        }
+
+        public int min() {
+            if (this.isEmpty()) {
+                return Integer.MAX_VALUE; // Error value
+            } else {
+                return peek().min;
+            }
+        }
+    }
+
+    static class NodeWithMin {
+        public int value;
+        public int min;
+        public NodeWithMin(int v, int min){
+            value = v;
+            this.min = min;
+        }
+    }
+
     public static void main(String[] args) {
 
+//        StackWithMin stack = new StackWithMin();
+        StackWithMin2 stack = new StackWithMin2();
+        stack.push(5);
+        stack.push(6);
+        stack.push(3);
+        stack.push(7);
+        System.out.println("min: "+stack.min());
+        stack.pop();
+        System.out.println("min: "+stack.min());
+        stack.pop();
+        System.out.println("min: "+stack.min());
+
+
     }
+
+    //    We can (maybe) do a bit better than this by using an additional stack which keeps track of the mins.
+    public static class StackWithMin2 extends Stack<Integer> {
+        Stack<Integer> s2;
+        public StackWithMin2() {
+            s2 = new Stack<Integer>();
+        }
+
+        public void push(int value){
+            if (value <= min()) {
+                s2.push(value);
+            }
+            super.push(value);
+        }
+
+        public Integer pop() {
+            int value = super.pop();
+            if (value == min()) {
+                s2.pop();
+            }
+            return value;
+        }
+
+        public int min() {
+            if (s2.isEmpty()) {
+                return Integer.MAX_VALUE;
+            } else {
+                return s2.peek();
+            }
+        }
+    }
+
 }
