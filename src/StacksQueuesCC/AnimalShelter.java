bec847b2d8a0f8aea8d67ed50a2893d63e93d22e
alexei <alexei.jobs@gmail.com>
2019-08-07 11:50:19 +0300

    Finished StacksQueuesCC with AnimalShelter

---
+++ src/StacksQueuesCC/AnimalShelter.java
@@ -0,0 +1,97 @@
+package StacksQueuesCC;
+
+/*3.6 Animal Shelter: An animal shelter, which holds only dogs andcats, operates on a strictly"first in, first
+        out"basis. People must adopt either the "oldest"(based on arrival time) ofall animals at the shelter,
+        or they can select whether they would prefer a dog or a cat (and will receive the oldest animal of
+        that type). They cannot select which specific animal they would like. Create the data structures to
+        maintain this system and implement operations such as enqueue, dequeueAny, dequeueDog,
+        and dequeueCat.You may use the built-in Linkedlist data structure.
+
+       pp 250 */
+
+import java.util.LinkedList;
+
+public class AnimalShelter {
+
+    abstract class Animal {
+        private int order;
+        protected String name;
+
+        public Animal(String n) {
+            name = n;
+        }
+
+        public void setOrder(int ord) {
+            order = ord;
+        }
+
+        public int getOrder() {
+            return order;
+        }
+
+        /* Compare orders of animals to return the older item. */
+        public boolean isOlderThan(Animal a) {
+            return this.order < a.getOrder();
+        }
+    }
+
+    class AnimalQueue {
+        LinkedList<Dog> dogs = new LinkedList<Dog>();
+        LinkedList<Cat> cats = new LinkedList<Cat>();
+        private int order = 0; // acts as timestamp
+
+        public void enqueue(Animal a) {
+                 /* Order is used as a sort of timestamp, so that we can compare the insertion
+                  * order of a dog to a cat. */
+            a.setOrder(order);
+            order++;
+
+            if (a instanceof Dog) dogs.addLast((Dog) a);
+            else if (a instanceof Cat) cats.addLast((Cat) a);
+        }
+
+
+        public Animal dequeueAny() {
+            /* Look at tops of dog and cat queues, and pop the queue with the oldest
+             * value. */
+            if (dogs.size() == 0) {
+                return dequeueCats();
+            } else if (cats.size() == 0) {
+                return dequeueDogs();
+            }
+            Dog dog = dogs.peek();
+            Cat cat = cats.peek();
+            if (dog.isOlderThan(cat)) {
+                return dequeueDogs();
+            } else {
+                return dequeueCats();
+            }
+        }
+
+        public Dog dequeueDogs() {
+            return dogs.poll();
+        }
+
+        public Cat dequeueCats() {
+            return cats.poll();
+        }
+
+
+    }
+
+    public class Dog extends Animal {
+        public Dog(String n) {
+            super(n);
+        }
+    }
+
+    public class Cat extends Animal {
+        public Cat(String n) {
+            super(n);
+        }
+    }
+}
+
+
+
+
