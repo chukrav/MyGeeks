10d636474d281935bb7bf0063ee2fee4680d39c6
lesha K <alexei.jobs@gmail.com>
2019-08-18 08:34:21 +0300

    BuildOrder started

--- src/TreesAndGraphsCC/BuildOrder.java
+++ src/TreesAndGraphsCC/BuildOrder.java
@@ -14,6 +14,7 @@ Output: f, e, a, b, d, c
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Stack;
 
 public class BuildOrder {
 
@@ -105,13 +106,76 @@ public class BuildOrder {
         public ArrayList<Project> getNodes() { return nodes; }
     }
 
-    public class Project {
+
+    // test was not implemented.
+    public static void main(String[] args) {
+
+    }
+
+    /*We also need a state to indicate "I've already processed/built this node" so we don't re-build the node. Our
+    state therefore can have three options: COMPLETED, PARTIAL, and BLANK.
+    The code below implements this algorithm.
+    Like the earlier algorithm, this solution is O (P+D) time, where P is the number of projects and D is the
+    number of dependency pairs.
+    By the way, this problem is called topological sort: linearly ordering the vertices in a graph such
+    that for every edge (a, b), a appears before b in the linear order.
+*/
+
+    Stack<Project> findBuildOrder2(String[] projects, String[][] dependencies) {
+        Graph graph= buildGraph(projects, dependencies);
+        return orderProjects2(graph.getNodes());
+    }
+
+    Stack<Project> orderProjects2(ArrayList<Project> projects){
+        Stack<Project> stack = new Stack<Project>();
+        for (Project project: projects){
+            if (project.getState() == Project.State.BLANK) {
+                if (!doDFS(project, stack)){
+                    return null;
+                }
+            }
+        }
+        return stack;
+    }
+
+    boolean doDFS(Project project, Stack<Project> stack){
+        if (project.getState() == Project.State.PARTIAL){
+            return false; // Cycle
+        }
+
+        if (project.getState() == Project.State.BLANK){
+            project.setState(Project.State.PARTIAL);
+            ArrayList<Project> children = project.getChildren();
+            for (Project child : children){
+                if (!doDFS(child, stack)){
+                    return false;
+                }
+            }
+            project.setState(Project.State.COMPLETE);
+            stack.push(project);
+        }
+        return true;
+    }
+
+    /* Same as before*/
+    //Graph buildGraph(String[] projects, String[][] dependencies){...}
+    //public class Graph{}
+
+    /* Essentially equivalent to earlier solution, with state info added and
+    * dependency count removed.*/
+
+
+    public static class Project {
         private ArrayList<Project> children = new ArrayList<Project>();
         private HashMap<String, Project> map= new HashMap<String, Project>();
         private String name;
         private int dependencies = 0;
 
         public Project(String n) { name = n; }
+        public enum State{COMPLETE, PARTIAL, BLANK};
+        private State state = State.BLANK;
+        public State getState(){ return state; }
+        public void setState(State st){ state = st; }
 
         public void addNeighbor(Project node) {
             if (!map.containsKey(node.getName())) {
@@ -128,8 +192,7 @@ public class BuildOrder {
         public ArrayList<Project> getChildren() { return children; }
         public int getNumberDependencies() { return dependencies; }
     }
-    // test was not implemented.
-    public static void main(String[] args) {
 
-    }
+
+
 }
