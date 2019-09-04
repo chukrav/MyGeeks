bf304594e7216e4bd9b3909fcd899752e8852a8f
lesha K <alexei.jobs@gmail.com>
2019-09-04 07:24:40 +0300

    FlipBitToWin optim add

--- src/BitManipulations/FlipBitToWin.java
+++ src/BitManipulations/FlipBitToWin.java
@@ -78,6 +78,26 @@ public class FlipBitToWin {
 however, reduce the memory usage.
 
      */
+    int flipBit(int a) {
+        /* If all ls, this is already the longest sequence. */
+        if (~a== 0) return Integer.BYTES * 8;
+
+        int currentLength = 0;
+        int previousLength= 0;
+        int maxlength = 1; // We can always have a sequence of at least one 1
+        while (a != 0) {
+            if ((a & 1) == 1) { // Current bit is a 1
+                currentLength++;
+            } else if ((a & 1) == 0) { // Current bit is a 0
+                /* Update to 0 (if next bit is 0) or currentLength (if next bit is 1). */
+                previousLength = (a & 2) == 0? 0 : currentLength;
+                currentLength = 0;
+            }
+            maxlength= Math.max(previousLength + currentLength + 1, maxlength);
+            a >>>= 1;
+        }
+        return maxlength;
+    }
 
 
 
