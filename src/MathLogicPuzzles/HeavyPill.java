382a58d0bcd581389e07eb5ef8e134f54913b60e
lesha K <alexei.jobs@gmail.com>
2019-09-23 06:06:45 +0300

    Ch 6 started

---
+++ src/MathLogicPuzzles/HeavyPill.java
@@ -0,0 +1,14 @@
+package MathLogicPuzzles;
+
+/*  6.1 The Heavy Pill:You have 20 bottles ofpills. 19 bottles have 1.0 gram pills, but one has pills of weight
+1.1 grams. Given a scale that provides an exact measurement, how would you find the heavy bottle?
+You can only use the scale once.
+
+
+          pp 300 */
+
+public class HeavyPill {
+
+
+
+}
